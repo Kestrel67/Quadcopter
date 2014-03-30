9b78351b2ae60370f60731acafe578bbc0f4971a
Dietrich Lucas <kestrel.3rd@gmail.com>
2014-03-30 21:42:03 +0200

    Quadcopter V1.32
    change http://www.infrarouges.fr/ to http://www.kestrel.fr/

--- ALN3D/Timer1Counter.cpp
+++ ALN3D/Timer1Counter.cpp
@@ -1,7 +1,7 @@
 /**
  * @author : Dietrich Lucas (Kestrel)
  * @email : lucas.dietrich.pro@gmail.com
- * @website http://www.infrarouges.fr
+ * @website http://www.kestrel.fr
  */
 
 #include <ALN3D.h>
